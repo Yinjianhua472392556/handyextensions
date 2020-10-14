3b0d61f36b04423b621df2f898e2333505cfada3
尹建华 <jianhuayin@thetiger.com.cn>
2020-10-14 11:04:38 +0800

    Initial Commit

--- Example/HandyExtensions/ViewController.swift
+++ Example/HandyExtensions/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import HandyExtensions
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        var text = ""
+        print(text.lalalla())
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ HandyExtensions/Classes/Model/StringExtension.swift
@@ -0,0 +1,17 @@
+//
+//  StringExtension.swift
+//  HandyExtensions
+//
+//  Created by Apple on 2020/10/14.
+//
+
+import UIKit
+
+
+extension String {
+    
+   public func lalalla() -> String {
+        return "lalalla"
+    }
+
+}
